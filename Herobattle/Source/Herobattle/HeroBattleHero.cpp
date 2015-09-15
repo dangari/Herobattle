16ae111a33cc2f2ed2b40fca2b1a43245c7d91df
Christoph Kümper <ckuemper@gmx.de>
2015-09-15 14:48:21 +0200

    update to 4.9 added follower

---
+++ Herobattle/Source/Herobattle/HeroBattleHero.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Herobattle.h"
+#include "HeroBattleHero.h"
+
+
+// Sets default values
+AHeroBattleHero::AHeroBattleHero()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHeroBattleHero::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHeroBattleHero::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AHeroBattleHero::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+

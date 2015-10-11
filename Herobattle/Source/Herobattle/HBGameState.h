ed883aa8227151084559456ec1f3ad5cf50d32b2
Christoph Kümper <ckuemper@gmx.de>
2015-10-11 16:01:32 +0200

    added Gamestateclass

---
+++ Herobattle/Source/Herobattle/HBGameState.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameState.h"
+#include "HBGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEROBATTLE_API AHBGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

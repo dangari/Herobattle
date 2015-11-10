75663cabb996266ef9027126015727c1b622d5a5
Christoph Kümper <ckuemper@gmx.de>
2015-11-10 15:46:18 +0100

    added fillScaleTable function to BaseSkillComponent

--- Herobattle/Source/Herobattle/Skills/Components/BaseSkillComponent.cpp
+++ Herobattle/Source/Herobattle/Skills/Components/BaseSkillComponent.cpp
@@ -32,13 +32,38 @@ void UBaseSkillComponent::init(FXmlNode* node)
 
 }
 
+void UBaseSkillComponent::fillScaleTable(FXmlNode* node)
+{
+	int standartValue = FCString::Atoi(*(node->GetAttribute(TEXT("value"))));
+	scaleTable[0] = standartValue;
+	scaleAttribute = SkillEnums::stringToAttribute(node->GetAttribute(TEXT("scaling")));
+	if (scaleAttribute != Attributes::NONE)
+	{
+		TArray<FXmlNode*> propertyList = node->GetChildrenNodes();
+		//reads the scale table values
+		for (auto& prop : propertyList)
+		{
+			int index = FCString::Atoi(*(prop->GetAttribute(TEXT("key"))));
+			int value = FCString::Atoi(*(prop->GetAttribute(TEXT("value"))));
+			scaleTable[index] = value;
+		}
+	}
+	// fills the empty spaces off the scaletable array
+	for (int Index = 0; Index > 19; ++Index)
+	{
+		int value1 = scaleTable[Index];
+		int value2 = scaleTable[Index + 1];
+		if (value2 == 0)
+		{
+			scaleTable[Index + 1] = value1;
+		}
+	}
+}
+
 UBaseSkillComponent* UBaseSkillComponent::MAKE()
 {
 	UBaseSkillComponent* w = NewObject<UBaseSkillComponent>();
 	return w;
 }
 
-RegisterComponent<UBaseSkillComponent> UBaseSkillComponent::reg(TEXT("test"));
-//template<typename T>
-//TMap<FString, classFuncPtr>  RegisterComponent<T>::map;
 

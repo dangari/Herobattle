33b95e51750d102f80f8effdcdb9772c4707d04f
Christoph Kümper <ckuemper@gmx.de>
2016-01-09 16:50:21 +0100

    finished charcter Blackboard

--- Herobattle/Source/Herobattle/AI/HBBlackboard.cpp
+++ Herobattle/Source/Herobattle/AI/HBBlackboard.cpp
@@ -6,3 +6,49 @@
 
 
 
+void UHBBlackboard::addAction(FString name, TArray<FSimAction> actionList)
+{
+	if (actionUserList.Contains(name))
+	{
+		FSimCharacter character = *actionUserList.Find(name);
+		character.actionList = actionList;
+	}
+	else
+	{
+		FSimCharacter character;
+		character.actionList = actionList;
+		actionUserList.Add(name, character);
+	}
+}
+
+TArray<FSimAction> UHBBlackboard::getActions(FString name)
+{
+	if (actionUserList.Contains(name))
+	{
+		FSimCharacter character = *actionUserList.Find(name);
+		return character.actionList;
+	}
+	else
+	{
+		TArray<FSimAction> emptyList;
+		return emptyList;
+	}
+}
+
+TArray<FSimAction> UHBBlackboard::getTargetAction(FString name)
+{
+	TArray<FSimAction> targetActionList;
+	for (auto& character : actionUserList)
+	{
+		TArray<FSimAction> list = character.Value.actionList;
+		for (auto& action : list)
+		{
+			if (action.targetName.Equals(name) || action.ownerName.Equals(name))
+			{
+				targetActionList.Add(action);
+			}
+		}
+	}
+	targetActionList.Sort(FSimAction::ConstPredicate);
+	return targetActionList;
+}
--- Herobattle/Source/Herobattle/AI/HBBlackboard.h
+++ Herobattle/Source/Herobattle/AI/HBBlackboard.h
@@ -3,17 +3,58 @@
 #pragma once
 
 #include "Object.h"
+#include "Enum/AIEnums.h"
+#include "Skills/Skill.h"
 #include "HBBlackboard.generated.h"
 
 /**
  * 
  */
+
+USTRUCT()
+struct FSimAction
+{
+
+	GENERATED_USTRUCT_BODY()
+
+	float time;
+	AIAction action;
+	FString targetName;
+	FString ownerName;
+	USkill* skill;
+
+	inline static bool ConstPredicate(const FSimAction ip1, const FSimAction ip2)
+	{
+		return (ip1.time > ip2.time);
+	}
+
+};
+
+USTRUCT()
+struct FSimCharacter
+{
+	GENERATED_USTRUCT_BODY()
+
+	TArray<FSimAction> actionList;
+
+};
+
 UCLASS()
 class HEROBATTLE_API UHBBlackboard : public UObject
 {
 	GENERATED_BODY()
-	
-	
-	
+public:
+
+	void addAction(FString name, TArray<FSimAction> actionList);
+
+	TArray<FSimAction> getActions(FString name);
+
+	TArray<FSimAction> getTargetAction(FString name);
+
+
+private:
+	TMap<FString, FSimCharacter> actionUserList;
+
+	TMap<FString, FSimCharacter> actionTargetList;
 	
 };
